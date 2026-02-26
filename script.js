3323de591f4c3ab7c628e34ad58bf3ada68cbf74
Md Sartaz <138593128+Mdsartaz@users.noreply.github.com>
2026-02-26 11:10:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openEnquiry() {
+    document.getElementById("enquiryBox").enquiry.display = "flex";
+}
+
+function closeEnquiry() {
+    document.getElementById("enquiryBox").enquiry.display = "none";
+}
